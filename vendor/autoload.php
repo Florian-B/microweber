2ee559c9aa6f58ce2207f3371af05f41f2bb0a72
Peter Ivanov <peter@microweber.com>
2014-06-09 16:45:57 +0300

    updates

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit258029c2cf9694cdbc02ea675a3ff90d::getLoader();
+return ComposerAutoloaderInit28a32583e17a5461c83c454398d8486b::getLoader();
